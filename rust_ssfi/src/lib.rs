ad2c14e23351692be29b11b43a99fcee874e6568
Christopher Edelmaier <christopher.edelmaier@gmail.com>
2015-07-01 16:17:51 -0600

    Holy crap comm works rust 1.3.0 6-30 build

--- rust_ssfi/src/lib.rs
+++ rust_ssfi/src/lib.rs
@@ -1,4 +1,4 @@
-//extern crate comm;
+extern crate comm;
 
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel};
@@ -6,16 +6,14 @@ use std::{thread};
 use std::collections::{HashMap,BTreeMap};
 use std::fs::{File};
 
-//use comm::{spmc};
+use comm::{spmc};
 
 pub fn ssfi() {
     // Start the queue
-    //let word_map: HashMap<&str, usize> = HashMap::new();
-    let word_map: BTreeMap<&str, usize> = BTreeMap::new();
+    let word_map: HashMap<&str, usize> = HashMap::new();
     let data = Arc::new(Mutex::new(word_map));
-    //let nthreads = 2;
-    //let (send, recv) = spmc::unbounded::new();
-    let (send, recv) = channel::<&str>();
+    let nthreads = 2;
+    let (send, recv) = spmc::unbounded::new();
     let input = vec!["The task is",
                      "to write a program which",
                      "accepts",
@@ -39,49 +37,34 @@ pub fn ssfi() {
         // and doesn't close prematurely
     });
 
-    // Run single threaded listeners for now
-    // Have to move inside of a scope to clone data
-    let data2 = data.clone();
-    let recv_guard = thread::spawn(move || {
-        while let Ok(n) = recv.recv() {
-            let words = n.split(' ');
-            for word in words {
-                let mut data = data2.lock().unwrap();
-                *data.entry(word).or_insert(0) += 1
-            }
-        }
-    });
-
 
     // Start the listeners
     // Use a JoinHandle to collect the threads
     // Then start listening, which is a blocking
     // operation.
-    //let recv_guards: Vec<_> = (0..nthreads).map( |i| {
-    //    let (recv, data) = (recv.clone(), data.clone());
-    //    thread::spawn(move || {
-    //        // Listen unless the sender has disconnected
-    //        while let Ok(n) = recv.recv_sync() {
-    //            let words = n.split(' ');
-    //            for word in words {
-    //                let mut data = data.lock().unwrap();
-    //                // Add the data to the map
-    //                //let counter = data.entry(word).or_insert(0);
-    //                //*counter += 1;
-    //                // BTreeMap in place manipulation
-    //                *data.entry(word).or_insert(0) += 1;
-    //            }
-    //        }
-    //    })
-    //}).collect();
+    let recv_guards: Vec<_> = (0..nthreads).map( |i| {
+        let (recv, data) = (recv.clone(), data.clone());
+        thread::spawn(move || {
+            // Listen unless the sender has disconnected
+            while let Ok(n) = recv.recv_sync() {
+                let words = n.split(' ');
+                for word in words {
+                    let mut data = data.lock().unwrap();
+                    // Add the data to the map
+                    //let counter = data.entry(word).or_insert(0);
+                    //*counter += 1;
+                    // BTreeMap in place manipulation
+                    *data.entry(word).or_insert(0) += 1;
+                }
+            }
+        })
+    }).collect();
 
     // Join the sender, then receivers
-    //println!("Sender: {:?}", send_guard.join().unwrap());
-    //for i in recv_guards {
-    //    println!("Receiver: {:?}", i.join().unwrap()); 
-    //}
     println!("Sender: {:?}", send_guard.join().unwrap());
-    println!("Receiver: {:?}", recv_guard.join().unwrap());
+    for i in recv_guards {
+        println!("Receiver: {:?}", i.join().unwrap()); 
+    }
 
     // What does the data look like?
     println!("Data:\n {:?}", data);
